5f03696c9d9f2260a04823ddb3fb233aeaf76404
Victor Souza <victorsouza2k@hotmail.com>
2022-03-19 00:27:36 -0300

    commit da terceira aula incompleta

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from './context/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
 
@@ -11,8 +12,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path='/' exact component={Home}></Route>
-        <Route path='/rooms/new' component={NewRoom}></Route>
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/rooms/new'  component={NewRoom}></Route>
+          <Route path='/rooms/:id' component={Room}></Route>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
